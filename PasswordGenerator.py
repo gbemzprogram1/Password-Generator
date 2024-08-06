7ce7985ff4292fddda858bd1f4706c99aefff9d9
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-08-06 14:46:57 -0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,5 @@
-Print('Welcome to the free Password Generator')
-Print('Your password requires a minimum of 12 characters')
-input('How many characters would you like your password to have? ')
+print('Welcome to the free Password Generator')
+print('Your password requires a minimum of 12 characters')
+characters =input('How many characters would you like your password to have? ')
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!', '#', '$', '%', '&', '(', ')', '*', '+']
+
