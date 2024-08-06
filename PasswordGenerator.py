b1b0cc3788ba0036e54eaa6e7d43b33aef0cd02b
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-08-06 15:38:27 -0400

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,32 @@
-print('Welcome to the free Password Generator')
-print('Your password requires a minimum of 12 characters')
-characters =input('How many characters would you like your password to have? ')
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!', '#', '$', '%', '&', '(', ')', '*', '+']
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+for char in range(1,nr_letters+1):
+    random_char =random.choice(letters)
+    password += random_char
+    
+    
+    
+for char in range (1,nr_symbols+1):
+    password += random.choice(symbols) 
+    
+    
+for char in range(1,nr_numbers+1):
+    password += random.choice(numbers)
+    random.shuffle(password)
+   
+    
+final_password = ''.join(password)
+print("Your generated password is:", final_password)
+       
+    
+    
